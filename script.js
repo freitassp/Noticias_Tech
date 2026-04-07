09268d9c99eb086933ba43f01beeaf6b358e05c8
Samuel Prieto Freitas <45234968+freitassp@users.noreply.github.com>
2026-04-07 17:01:24 -0300

    bghfh

---
+++ script.js
@@ -0,0 +1,8 @@
+// Criamos uma função chamada 'comprar'
+function comprar() {
+    // Exibe uma janelinha de alerta no navegador (Visto na pág. 11 do PDF)
+    alert("Produto adicionado ao carrinho com sucesso!");
+    
+    // Exibe uma mensagem escondida no 'Inspecionar' para o desenvolvedor
+    console.log("O usuário clicou no botão de compra.");
+}
