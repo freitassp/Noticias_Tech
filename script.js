3acb7f5e34cf1fbf7554b4786ecb9e54881e26da
Samuel Prieto Freitas <45234968+freitassp@users.noreply.github.com>
2026-04-07 17:12:07 -0300

    rtrtuytu

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-// Assim que a página carregar, isso deve aparecer
-alert("O JavaScript foi carregado com sucesso!");
-
+// Esta função será chamada quando o link for clicado
 function comprar() {
-    alert("Você clicou no botão!");
+    // 1. Isso vai travar a tela e você VAI ver
+    alert("Iniciando processo de compra...");
+    
+    // 2. Isso vai aparecer no F12 (Console)
+    console.log("O botão foi acionado com sucesso!");
 }
