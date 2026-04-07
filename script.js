838cabafaf1e596f06fac4f6940b7119fbd95163
Samuel Prieto Freitas <45234968+freitassp@users.noreply.github.com>
2026-04-07 17:06:49 -0300

    hfhf

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-// Criamos uma função chamada 'comprar'
+// Assim que a página carregar, isso deve aparecer
+alert("O JavaScript foi carregado com sucesso!");
+
 function comprar() {
-    // Exibe uma janelinha de alerta no navegador (Visto na pág. 11 do PDF)
-    alert("Produto adicionado ao carrinho com sucesso!");
-    
-    // Exibe uma mensagem escondida no 'Inspecionar' para o desenvolvedor
-    console.log("O usuário clicou no botão de compra.");
+    alert("Você clicou no botão!");
 }
